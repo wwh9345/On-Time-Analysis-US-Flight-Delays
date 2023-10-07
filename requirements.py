4002a0d4622af040e055913050e5007010920559
wwh9345 <92571055+wwh9345@users.noreply.github.com>
2023-10-08 01:01:09 +0800

    Added 'requirements.py'
    Installation codes for required libraries to run the Jupyter Notebook

---
+++ requirements.py
@@ -0,0 +1,10 @@
+# Required libraries
+
+pip install numpy
+pip install pandas
+pip install matplotlib.pyplot
+pip install seaborn
+pip install datetime
+pip install scipy
+pip install folium
+pip install sklearn
